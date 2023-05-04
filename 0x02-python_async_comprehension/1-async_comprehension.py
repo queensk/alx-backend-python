8e2306b8e029907e228778cd79d19aa75de11c95
queensk <queenskisivuli@gmail.com>
2023-05-05 02:25:17 +0300

    Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Async Comprehensions
+    """
+    return [i async for i in async_generator()]
