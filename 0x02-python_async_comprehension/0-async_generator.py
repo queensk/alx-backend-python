266115c845352fe4cc08548057ed2cbf004dda64
queensk <queenskisivuli@gmail.com>
2023-05-04 08:55:19 +0300

    include generetor

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Async Generator
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     Async Generator
     """
