41dd55cb8f063a517f346759543d63967951ec03
queensk <queenskisivuli@gmail.com>
2023-05-05 02:47:22 +0300

    Run time for four parallel comprehensions float return

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ from time import perf_counter
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Run time for four parallel comprehensions
     """
